1fda32e038311998a1b2b46df8ef947bfa57fd24
ARGAJA SHENDE <argaja10@gmail.com>
2024-06-24 18:10:38 -0500

    Add files via upload

--- stain_registration/normalize.py
+++ stain_registration/normalize.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 import os
@@ -31,32 +30,29 @@ def get_normalized_image(input_img_path):
     
     Returns:
         image (numpy.ndarray): Normalized image.
+        normalized_img_path (str): Path to the normalized image.
     """
     template_img = cv2.imread('./TestData/01-HE.jpg')
-    template_img = cv2.cvtColor(template_img,cv2.COLOR_BGR2LAB)
+    template_img = cv2.cvtColor(template_img, cv2.COLOR_BGR2LAB)
     template_mean, template_std = get_mean_and_std(template_img)
     
     input_img = cv2.imread(input_img_path)
-    input_img = cv2.cvtColor(input_img,cv2.COLOR_BGR2LAB)
+    input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2LAB)
     img_mean, img_std = get_mean_and_std(input_img)
     
-    height, width, channel = input_img.shape
-    for i in range(0,height):
-        for j in range(0,width):
-            for k in range(0,channel):
-                x = input_img[i,j,k]
-                x = ((x-img_mean[k])*(template_std[k]/img_std[k]))+template_mean[k]
-                x = round(x)
-                # boundary check
-                x = 0 if x<0 else x
-                x = 255 if x>255 else x
-                input_img[i,j,k] = x
-                
-    normalized_img= cv2.cvtColor(input_img,cv2.COLOR_LAB2BGR)
-    #plt.imshow(input_img)
+    # Vectorized operation for normalization
+    input_img = input_img.astype(np.float32)
+    input_img -= img_mean
+    input_img *= (template_std / img_std)
+    input_img += template_mean
+    input_img = np.clip(input_img, 0, 255)
+    input_img = input_img.round().astype(np.uint8)
+    
+    normalized_img = cv2.cvtColor(input_img, cv2.COLOR_LAB2BGR)
+    
     # Get the image name from the path
     img_name = os.path.basename(input_img_path)
-    normalized_img_path = './TestData/normalized-' + img_name
+    normalized_img_path = os.path.join('./TestData', f'normalized-{img_name}')
     cv2.imwrite(normalized_img_path, normalized_img)
     
     return normalized_img, normalized_img_path
@@ -64,13 +60,17 @@ def get_normalized_image(input_img_path):
     
 
 # Stain normalization of IHC markers
-
+print("Normalizing (1/3)...")
 normalized_img, normalized_img_path = get_normalized_image('./TestData/01-CC10.jpg')
 plt.imshow(normalized_img)
+plt.show()
 
+print("Normalizing (2/3)...")
 normalized_img, normalized_img_path = get_normalized_image('./TestData/01-CD31.jpg')
 plt.imshow(normalized_img)
+plt.show()
 
+print("Normalizing (3/3)...")
 normalized_img, normalized_img_path = get_normalized_image('./TestData/01-Ki67.jpg')
 plt.imshow(normalized_img)
-
+plt.show()
